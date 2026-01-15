0904e37a00478aa05ded6567e3b468a22f18038c
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-15 10:59:27 +0600

    added about and contact with specific layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>This is page components</h1>
+    </div>
+  );
+}
---
+++ src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BrachPage() {
+  return (
+    <div>
+      <h1>This is Brach page </h1>
+    </div>
+  );
+}
