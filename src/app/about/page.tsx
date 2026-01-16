ac7889ecb3f3a85da3724e2270e0de4d2390af38
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-16 11:44:54 +0600

    error & loading page handle

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,6 @@
-export default function AboutPage() {
+export default async function AboutPage() {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  throw new Error("Something went wrong");
   return (
     <div>
       <h1>This is About page components</h1>
