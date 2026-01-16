b74b37f8e6a2167fcc62966c0d8173584c8aa2c0
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-16 16:52:16 +0600

    added force dynamic for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
+export const dynamic = "force-dynamic";
+
 export default async function AboutPage() {
   await new Promise((resolve) => setTimeout(resolve, 4000));
-  throw new Error("Something went wrong");
+  // throw new Error("Something went wrong");
   return (
     <div>
       <h1>This is About page components</h1>
