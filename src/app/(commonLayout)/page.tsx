5348fba07eb46bb55d04159a8b608fdb10147e60
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-20 21:04:53 +0600

    added user service and get session through user service

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,20 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { userService } from "@/src/services/user.service";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
+  const { data } = await userService.getSession();
 
-  console.log(cookieStore.toString());
-
-  const res = await fetch("http://localhost:5000/api/auth/get-session", {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
-
-  const session = await res.json();
-  console.log(session);
+  console.log(data);
 
   return (
     <div>
