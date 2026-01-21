1c232c0897a003af3585fa5f9bafd144bcb2b077
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-21 09:41:55 +0600

    added proxy file

---
+++ src/proxy.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const proxy = (request: NextRequest) => {
+  console.log(request.url);
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/dashboard"],
+};
