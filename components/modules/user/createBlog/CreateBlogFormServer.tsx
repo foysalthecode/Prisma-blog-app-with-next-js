1b4bb0c24800972577f2ba74bc51517ac877fb57
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-25 12:02:10 +0600

    Handled post method using server action

--- components/modules/user/createBlog/CreateBlogFormServer.tsx
+++ components/modules/user/createBlog/CreateBlogFormServer.tsx
@@ -9,11 +9,46 @@ import {
 } from "@/components/ui/card";
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { env } from "@/src/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { toast } from "sonner";
+
+const API_URL = env.API_URL;
 
 export default function CreateBlogFormServer() {
   const createBlog = async (formData: FormData) => {
     "use server";
-    console.log(formData.get("title"));
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+    const tags = formData.get("tags") as string;
+
+    const blogData = {
+      title,
+      content,
+      tags: tags
+        .split(",")
+        .map((item) => item.trim())
+        .filter((item) => item != ""),
+    };
+
+    const cookieStore = await cookies();
+
+    const res = await fetch(`${API_URL}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        cookie: cookieStore.toString(),
+      },
+      body: JSON.stringify(blogData),
+    });
+
+    if(res.status){
+        redirect("/dashboard/create-blog?success")
+    }
+
+    // console.log(res);
   };
 
   return (
@@ -26,16 +61,21 @@ export default function CreateBlogFormServer() {
         <form id="blog-form" action={createBlog}>
           <FieldGroup>
             <Field>
-              <FieldLabel>Title</FieldLabel>
-              <Input type="text" name="title" />
+              <FieldLabel htmlFor="title">Title</FieldLabel>
+              <Input id="title" name="title" placeholder="Blog title" />
             </Field>
             <Field>
-              <FieldLabel>Content</FieldLabel>
-              <Input type="text" name="title" />
+              <FieldLabel htmlFor="content">Content</FieldLabel>
+              <Textarea
+                id="content"
+                name="content"
+                placeholder="write your blog"
+                required
+              ></Textarea>
             </Field>
             <Field>
-              <FieldLabel>Tags</FieldLabel>
-              <Input type="text" name="title" />
+              <FieldLabel htmlFor="tags">Tags (comma separeted)</FieldLabel>
+              <Input id="tags" name="tags" placeholder="web,ph" />
             </Field>
           </FieldGroup>
         </form>
