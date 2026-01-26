5e4c03a66faf67fd02f25352d36c32c6f1679d9d
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-26 10:35:39 +0600

    navigate to next page -> pagination

---
+++ components/ui/pagination-controls.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
+import { Button } from "./button";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface PaginationControlsProps {
+  meta: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
+
+export default function PaginationControls({ meta }: PaginationControlsProps) {
+  const { page, limit, total, totalPages } = meta;
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const navigateToPage = (page: number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
+  };
+
+  return (
+    <div className="flex items-center justify-between px-2 py-4 border-t mt-4">
+      <div className="text-sm text-muted-foreground">
+        Showing 0 to 0 of 0 results
+      </div>
+
+      <div className="flex items-center space-x-2">
+        <Button variant="outline" size="icon">
+          <ChevronsLeft className="h-4 w-4" />
+        </Button>
+
+        <Button variant="outline" size="icon">
+          <ChevronLeft className="h-4 w-4" />
+        </Button>
+
+        <div className="flex items-center gap-1">
+          <span className="text-sm font-medium">Page 0 of 0</span>
+        </div>
+
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() => navigateToPage(page + 1)}
+        >
+          <ChevronRight className="h-4 w-4" />
+        </Button>
+
+        <Button variant="outline" size="icon">
+          <ChevronsRight className="h-4 w-4" />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/history/page.tsx
@@ -1,4 +1,5 @@
 import HistoryTable from "@/components/modules/user/history/HistoryTable";
+import PaginationControls from "@/components/ui/pagination-controls";
 import { blogService } from "@/src/services/blog.service";
 
 export default async function HistoryPage({
@@ -10,10 +11,18 @@ export default async function HistoryPage({
   const res = await blogService.getBlogPost({ page });
   const posts = res.data?.data || [];
 
+  const pagination = res.data?.pagination || {
+    page: 1,
+    limit: 10,
+    total: 0,
+    totalPages: 1,
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-6">BLog post History</h1>
       <HistoryTable posts={posts}></HistoryTable>
+      <PaginationControls meta={pagination} />
     </div>
   );
 }
